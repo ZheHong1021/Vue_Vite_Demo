5b9bf51524ce6809c367a99544c6a612ea01e3c0
ZheHong1021 <zhehome1021@gmail.com>
2024-10-10 21:08:11 +0800

    設定一個 @images的別名

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      // 添加 @ 别名(指向 src 目錄)
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+
+      // 添加 @images 别名(指向 src/assets/images 目錄)
+      '@images': fileURLToPath(new URL('./src/assets/images', import.meta.url))
     }
   }
 })
