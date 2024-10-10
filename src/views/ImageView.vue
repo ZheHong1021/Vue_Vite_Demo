1cf1865d72095471db0dc7d90ca5efe6b309d2e0
zhe-hong-Lin <58475836+ZheHong1021@users.noreply.github.com>
2024-10-10 21:16:15 +0800

    Test/vite config alias (#1)
    * 建立一個新的View的Router以及Link
    
    * 設定一個 @images的別名
    
    * 添加圖片並且在ImageView中實現使用@images的alias

---
+++ src/views/ImageView.vue
@@ -0,0 +1,13 @@
+<script setup>
+// 建立一個 imageSrc 變數，用來儲存圖片的路徑
+// 使用 URL 物件建立一個新的 URL，並將路徑轉換為絕對路徑
+const imageSrc = new URL('@images/工程吉娃娃.png', import.meta.url).href
+</script>
+<template>
+  <div>
+    <h1>Image View</h1>
+    <img :src="imageSrc" alt="工程吉娃娃" />
+  </div>
+</template>
+
+<style></style>
